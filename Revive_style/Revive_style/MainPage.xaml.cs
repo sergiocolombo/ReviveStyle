6ffef39da7509daa5c038d308db3051a5ee4752c
Sergio <sergio.colombo@issgreppi.it>
2021-10-20 10:59:16 +0200

    Hamburger Navigation Completa

--- Revive_style/Revive_style/MainPage.xaml.cs
+++ Revive_style/Revive_style/MainPage.xaml.cs
@@ -25,16 +25,35 @@ namespace Revive_style
         public MainPage()
         {
             this.InitializeComponent();
+            Titolo.Text = "Home";
+            ShopListItem.IsSelected = true;
+            MyFrame.Navigate(typeof(ZonaVendita));
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (ShopListItem.IsSelected)
+            {
+                MyFrame.Navigate(typeof(ZonaVendita));
+                Titolo.Text = "Shop";
+                MySplitView.IsPaneOpen = false;
+            }
+            else if (InfoListItem.IsSelected)
+            {
+                MyFrame.Navigate(typeof(Info));
+                Titolo.Text = "Info";
+                MySplitView.IsPaneOpen = false;
+            }
         }
 
         private void CarrelloButton_Click(object sender, RoutedEventArgs e)
         {
+            MyFrame.Navigate(typeof(Carrello));
+        }
 
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
         }
     }
 }
