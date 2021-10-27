9fd587600beb0c1582e2c3f480e13c52a513f791
Sergio <sergio.colombo@issgreppi.it>
2021-10-27 17:32:41 +0200

    visualizzazione immagine

--- Revive_style/Revive_style/VisualizzazioneCapo.xaml.cs
+++ Revive_style/Revive_style/VisualizzazioneCapo.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
@@ -28,9 +29,13 @@ namespace Revive_style
             Nome.Text = App.selezionato.nome;
             Taglia.Text = App.selezionato.taglia;
             Descrizione.Text = App.selezionato.descrizione;
-            Prezzo.Text = App.selezionato.prezzo;
+            Prezzo.Text = App.selezionato.prezzo.ToString();
+
+            Uri imageUri = (new Uri(base.BaseUri, @App.selezionato.indirizzoImmagine));
+            BitmapImage source = new BitmapImage(imageUri);
+            ImmagineCapo.Source = source;
         }
-       
+
         private void AddCarrello_Click(object sender, RoutedEventArgs e)
         {
 
